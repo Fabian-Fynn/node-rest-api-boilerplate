7345aae5c0cb65dd69839fed538a30df002a4904
rudolfsonjunior <rudolfson.junior@gmail.com>
2017-03-20 16:02:55 +0100

    :hammer: Refactor: delete empty objects

--- app/models/UserModel.js
+++ app/models/UserModel.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
+
 const Schema = mongoose.Schema;
-mongoose.models = {};
-mongoose.modelSchemas = {};
 
 const userSchema = new Schema({
-  name: String
+  name: String,
 });
 
 const User = mongoose.model('User', userSchema);
+
 module.exports = User;
