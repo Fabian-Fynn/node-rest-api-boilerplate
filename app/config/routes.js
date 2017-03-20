d09d62809b96a989b7af9971b4625e2292597dc4
rudolfsonjunior <rudolfson.junior@gmail.com>
2017-03-20 16:01:22 +0100

    :art: Style: intentation for :

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,9 +1,9 @@
 const routes = {
 
-  'POST /user'          : 'UserController.create',
-  'GET  /users'         : 'UserController.getAll',
-  'GET  /user/:user_id' : 'UserController.get'
+  'POST /user': 'UserController.create',
+  'GET /users': 'UserController.getAll',
+  'GET /user/:user_id': 'UserController.get',
 
-}
+};
 
 export default routes;
