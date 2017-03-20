9431e6f17b83287ca4886a3897cdfbefc126df75
rudolfsonjunior <rudolfson.junior@gmail.com>
2017-03-20 16:00:41 +0100

    :art: Style: imports alphabetical, const db

--- app/app.js
+++ app/app.js
@@ -1,12 +1,11 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 import helmet from 'helmet';
 import http from 'http';
-import bodyParser from 'body-parser';
-import path from 'path';
-import { defaultPort } from './config/config';
+import mapRoutes from 'express-routes-mapper';
 import mongoose from 'mongoose';
 import routes from './config/routes';
-import mapRoutes from 'express-routes-mapper';
+import defaultPort from './config/config';
 
 const app = express();
 app.use(helmet({
@@ -16,14 +15,14 @@ app.use(helmet({
 }));
 const server = http.Server(app);
 const port = process.env.PORT || defaultPort;
-const db = mongoose.connect('mongodb://localhost/default');
+mongoose.connect('mongodb://localhost/default');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/api', mapRoutes(routes));
 
-server.listen(port, function() {
+server.listen(port, () => {
   console.log('There we go ♕');
   console.log(`Gladly listening on http://127.0.0.1:${port}`);
 });
